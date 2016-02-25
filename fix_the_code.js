994748201d82c333a0d4fb23469edefd8824e640
zuphu <anthony.guev@gmail.com>
2016-02-24 23:44:54 -0500

    Finished fix the code in javascript.

--- fix_the_code.js
+++ fix_the_code.js
@@ -1,24 +1,31 @@
 // Render HTML with a receipt for an order
+// Arguments: order {object}
 function receipt(order) {
   var p = 'Payment info: ';
-    
-  if (!order || !order.payment_type) {
-    return; // Return if undefined to prevent runtime errors 
+
+  //add a check to prevent run time errors if values are undefined
+  if (!order || || !order.payment || !order.payment_type) {
+    return;
   }
 
-  if (order.payment_type == "creditcard") { // changed conditional equality to assignment operator
-    p += order.payment.getCardType + " " + order.payment.card_number; //card type(VISA/MasterCard etc.) and number)
-  } else if (order.payment_type == "paypal") {
-    p += order.payment.paypal_info;
-  } else if (order.payment_type == "manual") {
-    p += order.payment.manual_payment_info;
-  } else if (order.payment_type == "free") {
-    p += "This order was free!"; //Free order or promotional item
+  if (order.payment_type === "creditcard" && order.payment.getCardType &&
+      order.payment.card_number) {
+    p = order.payment.getCardType + " " + order.payment.card_number; //card type(VISA/MasterCard etc.) and number)
+  } else if (order.payment_type === "paypal" && order.payment.paypal_info) {
+    p = order.payment.paypal_info;
+  } else if (order.payment_type === "manual" && order.payment.manual_payment_info) {
+    p = order.payment.manual_payment_info;
+  } else if (order.payment_type === "free") {
+    p = "This order was free!"; //Free order or promotional item
+  } else if (order.payment.default_payment_info) {
+    p = order.payment.default_payment_info;//default order info
   } else {
-    p += order.payment.default_payment_info;//default order info
+    p = 'Order type is invalid.'
+    document.write(p);
+    return;
   }
-  
-  if (order.payment_type != "free") {
+
+  if (order.payment_type !== "free" && order.amount_in_dollars) {
      p += "<p> was charged " + order.amount_in_dollars + "$" + "</p>";
   }
  
